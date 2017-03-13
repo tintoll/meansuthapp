7ae52e18361f5c562daf5fb9db5cfe1f207c6967
tintoll <blue@tigrison.com>
2017-03-13 13:16:46 +0900

    User Model 추가 및 User 저장 추가

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // Register
-router.get('/register', (req, res) => {
-    res.send('Register');
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        "name" : req.body.name,
+        "email" : req.body.email,
+        "username" : req.body.username,
+        "password" : req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err) {
+            res.json({ success : false,msg : 'Failed to User' });
+        } else {
+            res.json({ success : true,msg : 'User Registered' });
+        }
+    });
+
 });
 
 // Authenticate
-router.post('/authenticate', (req, res) => {
+router.post('/authenticate', (req, res, next) => {
     res.send('authenticate');
 });
 
 // Profile
-router.get('/profile', (req, res) => {
+router.get('/profile', (req, res, next) => {
     res.send('profile');
 });
 
-// Validate
-router.get('/validate', (req, res) => {
-    res.send('Validate');
-});
 
 
 module.exports = router;
