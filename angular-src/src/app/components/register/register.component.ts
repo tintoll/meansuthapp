d8b59e584f1cbb7ef8261c9a2e25bc271adfe913
tintoll <blue@tigrison.com>
2017-03-23 13:06:13 +0900

    회원가입 화면 validate 기능 추가

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService} from '../../services/validate.service';
+
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name : String;
+  username : String;
+  email : String;
+  password : String;
 
-  constructor() { }
+  constructor(private validateService : ValidateService,
+              private flashMessagesService : FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+    const user = {
+      name : this.name,
+      username : this.username,
+      email : this.email,
+      password : this.password
+    }
+
+    if(!this.validateService.validateRegister(user)){
+      this.flashMessagesService.show('값을 채워주세요',{cssClass:'alert-danger', timeout:3000});
+      return false;
+    }
+    if(!this.validateService.validateEmail(user.email)){
+      this.flashMessagesService.show('비밀번호 형식이 잘못되었습니다.',{cssClass:'alert-danger', timeout:3000});
+      return false;
+    }
+
   }
 
 }
