07be385b87b1b36bbd2aa476440474bc35f9dbf0
tintoll <belselios@gmail.com>
2017-03-25 00:46:58 +0900

    프로파일 화면 추가 및 로그인상태별로 화면 제어 추가

--- app.js
+++ app.js
@@ -48,6 +48,11 @@ app.get('/' ,(req, res) => {
     res.send('Invalid Endpoint');
 });
 
+// 모든 get요청일때 index.html이 동작하도록
+app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname,'public/index.html'));
+});
+
 // Start Server
 app.listen(port,() => {
     console.log('Server started on port '+port);
